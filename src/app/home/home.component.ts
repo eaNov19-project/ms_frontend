b15977a15f388b0bcef1cb65e20302cf782601d5
thutruongvn <thutruong.1986@gmail.com>
2019-11-20 21:58:27 -0600

    added question post at home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from '../services/question.service';
 import { Question, QuestionsResult } from '../models/question.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 
 @Component({
@@ -10,14 +12,44 @@ import { Question, QuestionsResult } from '../models/question.model';
 })
 export class HomeComponent implements OnInit {
 
+  public questionForm: FormGroup;
   questions: Array<Question> = [];
 
   constructor(private questionService: QuestionService) { }
-
-  ngOnInit() {
+  getAllQuestions(){
     this.questionService.getAllQuestions().subscribe(result => {
       this.questions = result.data.questions.slice(0, 7);
     });
+
+  }
+  ngOnInit() {
+    this.questionForm = new FormGroup({
+      title: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(200)
+      ]),
+      body: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(500)
+      ])
+    });
+    this.getAllQuestions();
   }
 
+  OnSubmitQuestionForm() {
+    for (const i in this.questionForm.controls) {
+      this.questionForm.controls[i].markAsDirty();
+      this.questionForm.controls[i].updateValueAndValidity();
+    }
+    this.questionService.addQuestion(this.questionForm.value)
+    .pipe(first())
+    .subscribe(
+      (result) => {
+        console.log(result);
+        this.questionForm.reset();
+        this.getAllQuestions();
+      },
+      err => console.log(err)
+    );
+  }
 }
